9db9600d06d56ce33663fbab64fe296bc5996e20
MiataBoy <miataboymx@gmail.com>
2023-02-23 12:24:30 +0100

    refactor

--- codewars/Python/kata/54bf1c2cd5b56cc47f0007a1.py
+++ codewars/Python/kata/54bf1c2cd5b56cc47f0007a1.py
@@ -1,16 +1,17 @@
-def duplicate_count(text):
-    duplicates = 0
-    ignoreList = []
-    text = text.lower()
-    for char in text:
-        if text.count(char) > 1 and char not in ignoreList:
-            duplicates += 1
-            ignoreList.append(char)
-    return duplicates
-
-print(duplicate_count('aA11'))
-
 # Part of KATA 54bf1c2cd5b56cc47f0007a1
 # https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/train/python
 # Goal is to find the amount of duplicates in a string.
 # aabcdeb should return 2.
+
+def duplicate_count(text):
+    duplicates = 0
+    ignorelist = []
+    text = text.lower()
+    for char in text:
+        if text.count(char) > 1 and char not in ignorelist:
+            duplicates += 1
+            ignorelist.append(char)
+    return duplicates
+
+
+print(duplicate_count('aA11'))
--- codewars/Python/kata/562926c855ca9fdc4800005b.py
+++ codewars/Python/kata/562926c855ca9fdc4800005b.py
@@ -1,3 +1,8 @@
+# Part of KATA 562926c855ca9fdc4800005b
+# https://www.codewars.com/kata/562926c855ca9fdc4800005b/train/python
+# Goal is to display the result of a specified number to its specified power
+# number_to_pwr(3,3) should return 27
+
 def number_to_pwr(number, p):
     result = 1
     for i in range(p):
